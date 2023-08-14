4783ba508c2d4933bfc94c15bdaa5224c488f63a
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-08-14 12:43:17 -0400

    changed app js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
 import './App.css';
-import { useState, Suspense, useEffect } from 'react'
+import { useState, Suspense, useRef } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ArtistView from './components/ArtistView'
+import AlbumView from './components/AlbumView'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import Spinner from './components/spinner'
+import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
 import { createResource as fetchData } from './helper'
 
-function App() {
-  let [searchTerm, setSearchTerm] = useState('')
-  let [message, setMessage] = useState('Search for Music!')
+const App = () => {
+  let searchInput = useRef('')
   let [data, setData] = useState(null)
-
-  useEffect(() => {
-    if (searchTerm) {
-      document.title=`${searchTerm} Music`
-      console.log(fetchData(searchTerm))
-      setData(fetchData(searchTerm))
-  }
-  }, [searchTerm])
+  let [message, setMessage] = useState('Search for Music!')
 
   const handleSearch = (e, term) => {
     e.preventDefault()
-    setSearchTerm(term)
+    setData(fetchData(term, 'main'))
   }
 
   const renderGallery = () => {
-    if(data){
+    if(data) {
       return (
         <Suspense fallback={<Spinner />}>
-          <Gallery data={data} />
+          <Gallery />
         </Suspense>
       )
     }
@@ -35,12 +32,25 @@ function App() {
 
   return (
     <div className="App">
-      <SearchBar handleSearch={handleSearch} />
       {message}
-      {renderGallery()}
+      <Router>
+        <Route exact path={'/'}>
+          <SearchContext.Provider value={{term: searchInput, handleSearch: handleSearch}}>
+            <SearchBar />
+          </SearchContext.Provider>
+            <DataContext.Provider value={data}>
+              {renderGallery()}
+            </DataContext.Provider>
+        </Route>
+        <Route path="/album/:id">
+          <AlbumView />
+        </Route>
+        <Route path="/artist/:id">
+          <ArtistView />
+        </Route>
+      </Router>
     </div>
-)
-
+  );
 }
 
 export default App;
