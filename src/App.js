6aba7eb32814242c531c56354dfd5860f227d2ea
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-08-14 12:33:39 -0400

    suspense

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import { useEffect, useState, Suspense } from 'react'
-import Gallery from './components/Gallery';
-import SearchBar from './components/SearchBar';
-import { createResource as fetchData } from './helper';
-import { Spinner } from './spinner';
-
+import './App.css';
+import { useState, Suspense, useEffect } from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import Spinner from './components/spinner'
+import { createResource as fetchData } from './helper'
 
 function App() {
-  let [message, setMessage] = useState('Seach for music')
-  let [search, setSearch] = useState('')
+  let [searchTerm, setSearchTerm] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState(null)
 
   useEffect(() => {
     if (searchTerm) {
-        setData(fetchData(searchTerm))
-    }
-}, [searchTerm])
+      document.title=`${searchTerm} Music`
+      console.log(fetchData(searchTerm))
+      setData(fetchData(searchTerm))
+  }
+  }, [searchTerm])
 
   const handleSearch = (e, term) => {
-    e.preventDefault();
-    setSearch(term)
+    e.preventDefault()
+    setSearchTerm(term)
   }
 
   const renderGallery = () => {
-    if(data) {
-        return (
-            <Suspense fallback={<Spinner />}>
-                <Gallery data={data} />
-            </Suspense>
-        )
+    if(data){
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      )
     }
-}
-
+  }
 
   return (
     <div className="App">
-        <SearchBar handleSearch={handleSearch} />
-        {message}
-        {renderGallery()}
+      <SearchBar handleSearch={handleSearch} />
+      {message}
+      {renderGallery()}
     </div>
-)
-
-
+  );
 }
+
 export default App;
